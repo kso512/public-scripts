67b3ab6e1ea0c3c9275136b46cb3d497dc0ea3c0
Chris Lindbergh <chrislindbergh@gmail.com>
2020-09-16 15:52:21 -0600

    Fill in mpd-nope code

--- mpd/mpd-nope.bash
+++ mpd/mpd-nope.bash
@@ -8,7 +8,32 @@
 FILE_INCLUDE="/home/mpd/mpd-include.txt"
 
 ## Find out what the currently playing song is
+if [ $(which mpc) ]
+  then
+    echo "Querying current song..."
+    CURRENT_SONG=$(mpc current -f %file%)
+    echo "Current song is:" $CURRENT_SONG
+  else
+    echo "Unable to query current song because mpc is missing!"
+    exit 1
+fi
 
 ## Remove that line from the include file
+if [ $(which sed) ]
+  then
+    echo "Removing current song from include file..."
+    sed -i 's~'"$CURRENT_SONG"'~~g' $FILE_INCLUDE
+  else
+    echo "Unable to remove a line from include file because sed is missing!"
+    exit 1
+fi
 
 ## Jump to the next song
+if [ $(which mpc) ]
+  then
+    echo "Jumping to next song..."
+    mpc next
+  else
+    echo "Unable to jump to next song because mpc is missing!"
+    exit 1
+fi
